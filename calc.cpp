55a3e483d3567a803712a34dd09622e25cdadbc3
Anatolich <myronenko.anatoliu@gmail.com>
2022-03-14 18:19:02 +0200

    exponentiation

--- calc.cpp
+++ calc.cpp
@@ -8,7 +8,7 @@ int main() {
 
   cout << "Введіть перше число\n";   cin >> a;
 
-  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";  cin >> op;
+  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n5\t^\n\n";  cin >> op;
 
   cout << "Введіть друге число\n";     cin >> b;
 
@@ -29,6 +29,10 @@ int main() {
     case 4:     // code
 	result = a / b;
     break;
+	
+	case 5; 	// code
+	result = pow(a,b);
+	break;
 
   }
 
